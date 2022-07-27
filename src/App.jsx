17107821315991e41266c336deb03a8f7121a6c5
Gustavo <gustavolima@unifei.edu.br>
2022-07-27 15:23:10 -0300

    chapter 2 - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+import './styles.css';
 
 function App() {
   return (
     <>
+      <Header />
       <Post 
         author="John Doe" 
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
